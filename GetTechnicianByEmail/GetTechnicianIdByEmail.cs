e3c208054ce70c5eed0f9d3323f8548026e5624d
Matt Collins <mcollins2618@gmail.com>
2018-11-15 13:03:49 -0800

    instance removal

--- GetTechnicianByEmail/GetTechnicianIdByEmail.cs
+++ GetTechnicianByEmail/GetTechnicianIdByEmail.cs
@@ -24,7 +24,7 @@ namespace GetTechnicianByEmail
             .FirstOrDefault(q => string.Compare(q.Key, "email", true) == 0)
             .Value;
 
-            var connectionString = @"AuthType=Office365;Url=https://fieldconnectdevelopment.crm.dynamics.com/;Username=<username>;Password=<password>";
+            var connectionString = @"AuthType=Office365;Url=https://<instance>.crm.dynamics.com/;Username=<username>;Password=<password>";
             CrmServiceClient conn = new CrmServiceClient(connectionString);
 
             IOrganizationService _orgService;
